1ebe3046468c71137978f5ab86410af344c8ad82
Bartosz Łepski <lepski.bartosz@gmail.com>
2020-03-17 22:18:54 +0100

    Add for loops to remove active class for links and articles

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
-  /* remove class 'active' from all article links  */
 
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  console.log(activeLinks);
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* remove class 'active' from all articles */
 
